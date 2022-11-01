37affef2873f564cd5595a3f786680abb09cad67
kartik-99-ux <kartikpandeyofficial@gmail.com>
2022-11-01 21:15:08 +0530

    my first commit

---
+++ abc.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class abc {
+    public static void main(String arg[])
+    { Scanner obj=new Scanner(System.in);
+        String s=new String();
+        s=obj.next();
+        char[] ch=new char[s.length()];
+        for(int i=0;i<s.length();i++)
+        {   
+           char[] c=s.toCharArray();
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            System.out.print(ch[i]);
+        }
+       
+    }
+}
